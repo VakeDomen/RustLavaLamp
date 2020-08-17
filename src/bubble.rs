c5f8ef8e976043237d207f8d3c4ea7d116b382b6
Vake Domen <vake.domen94@gmail.com>
2020-08-17 20:32:24 +0200

    implemented animation

---
+++ src/bubble.rs
@@ -0,0 +1,81 @@
+const RADIUS_ANIMATION_SPEED: f64 = 0.3;
+const BUBBLE_RADUIS_RANGE: f64 = 40.0;
+const BUBBLE_SPEED_RANGE: f64 = 3.0;
+const MIN_BUBBLE_RADIUS: f64 = 30.0;
+const MIN_BUBBLE_SPEED: f64 = 2.0;
+
+
+pub struct Bubble {
+    pub x: f64,
+    pub y: f64,
+    pub r1: f64,
+    pub r2: f64,
+    pub speed: f64,
+    pub original_r1: f64,
+    pub original_r2: f64,
+    pub r1_asc: bool,
+    pub r2_asc: bool,
+    pub r1_target: f64,
+    pub r2_target: f64
+}
+
+impl Bubble {
+    pub fn new(x: f64, y: f64, r1: f64, r2: f64, speed: f64) -> Bubble {
+        let mut bubble = Bubble {
+            x,
+            y,
+            r1: r1.clone() * BUBBLE_RADUIS_RANGE + MIN_BUBBLE_RADIUS,
+            r2: r2.clone() * BUBBLE_RADUIS_RANGE + MIN_BUBBLE_RADIUS,
+            speed: speed * BUBBLE_SPEED_RANGE + MIN_BUBBLE_SPEED,
+            original_r1: r1.clone() * BUBBLE_RADUIS_RANGE + MIN_BUBBLE_RADIUS,
+            original_r2: r2.clone() * BUBBLE_RADUIS_RANGE + MIN_BUBBLE_RADIUS,
+            r1_asc: false,
+            r2_asc: false,
+            r1_target: r2 * BUBBLE_RADUIS_RANGE + MIN_BUBBLE_RADIUS,
+            r2_target: r1 * BUBBLE_RADUIS_RANGE + MIN_BUBBLE_RADIUS,
+        };
+        if bubble.r1 < bubble.r2 {
+            bubble.r1_asc = true;
+        } else {
+            bubble.r2_asc = true;
+        }
+        bubble
+    }
+
+    pub fn update(&mut self) {
+        self.y -= self.speed;
+        if self.y < -self.r1 && self.y < self.r2 {
+            self.y = 600.0;
+        }
+
+        if !(self.r1 == self.r2 && self.r1_asc == self.r2_asc) {
+            let r_dff = (self.original_r1 - self.original_r2).abs();
+            if self.r1_asc {
+                self.r1 += RADIUS_ANIMATION_SPEED;
+                if self.r1 >= self.r1_target {
+                    self.r1_target -= r_dff;
+                    self.r1_asc = !self.r1_asc;
+                }
+            } else {
+                self.r1 -= RADIUS_ANIMATION_SPEED;
+                if self.r1 <= self.r1_target {
+                    self.r1_target += r_dff;
+                    self.r1_asc = !self.r1_asc;
+                }
+            }
+            if self.r2_asc {
+                self.r2 += RADIUS_ANIMATION_SPEED;
+                if self.r2 >= self.r2_target {
+                    self.r2_target -= r_dff;
+                    self.r2_asc = !self.r2_asc;
+                }
+            } else {
+                self.r2 -= RADIUS_ANIMATION_SPEED;
+                if self.r2 >= self.r2_target {
+                    self.r2_target += r_dff;
+                    self.r2_asc = !self.r2_asc;
+                }
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,91 @@
+extern crate piston_window;
+extern crate rand;
+mod bubble;
+
+use piston_window::*;
+use bubble::Bubble;
+
+const WIDTH: u32 = 1000;
+const HEIGHT: u32 = 500;
+const BG_COLOR: [f32; 4] = [245.0/255.0, 177.0/255.0, 88.0/255.0, 1.0];
+const INTERPOLATION_STEPS: u16 = 50;
+
+fn main() {
+    
+    let mut window: PistonWindow = WindowSettings::new("Lava lamp", Size::from((WIDTH, HEIGHT)))
+        .build()
+        .unwrap();
+    let mut bubbles: Vec<Bubble> = generate_bubbles(100);
+    let mut events = window.events.max_fps(30);
+
+    let mut color_of_bubbles: [f32; 4] = generate_random_color();
+    let mut target_color_of_bubbles: [f32; 4] = generate_random_color();
+
+    let mut interpolation_step_R: f32 = (target_color_of_bubbles[0] - color_of_bubbles[0]) / INTERPOLATION_STEPS as f32;
+    let mut interpolation_step_G: f32 = (target_color_of_bubbles[1] - color_of_bubbles[1]) / INTERPOLATION_STEPS as f32;
+    let mut interpolation_step_B: f32 = (target_color_of_bubbles[2] - color_of_bubbles[2]) / INTERPOLATION_STEPS as f32;
+    let mut interpolation_step_ALPHA: f32 = (target_color_of_bubbles[3] - color_of_bubbles[3]) / INTERPOLATION_STEPS as f32;
+
+
+    while let Some(e) = events.next(&mut window) {
+            
+            // render
+            if let Some(_) = e.render_args() {
+                window.draw_2d(&e, |c, g, _| { 
+                    let bubs = &bubbles;
+
+                    color_of_bubbles[0] += interpolation_step_R;
+                    color_of_bubbles[1] += interpolation_step_G;
+                    color_of_bubbles[2] += interpolation_step_B;
+                    color_of_bubbles[3] += interpolation_step_ALPHA;
+
+                    clear(BG_COLOR, g);
+                    for b in bubs.iter() {
+                        ellipse(
+                            color_of_bubbles,
+                            [b.x - b.r1, b.y - b.r2, b.r1 * 2.0, b.r2 * 2.0], 
+                            c.transform, 
+                            g
+                        );
+                    }
+
+                    if (target_color_of_bubbles[0] - color_of_bubbles[0]).abs() < 0.01 && 
+                        (target_color_of_bubbles[1] - color_of_bubbles[1]).abs() < 0.01 && 
+                        (target_color_of_bubbles[2] - color_of_bubbles[2]).abs() < 0.01 && 
+                        (target_color_of_bubbles[3] - color_of_bubbles[3]).abs() < 0.01 
+                    {
+                        target_color_of_bubbles = generate_random_color();
+                        interpolation_step_R = (target_color_of_bubbles[0] - color_of_bubbles[0]) / INTERPOLATION_STEPS as f32;
+                        interpolation_step_G = (target_color_of_bubbles[1] - color_of_bubbles[1]) / INTERPOLATION_STEPS as f32;
+                        interpolation_step_B = (target_color_of_bubbles[2] - color_of_bubbles[2]) / INTERPOLATION_STEPS as f32;
+                        interpolation_step_ALPHA = (target_color_of_bubbles[3] - color_of_bubbles[3]) / INTERPOLATION_STEPS as f32;
+                    }
+                });
+            };
+            // update
+            if let Some(_) = e.update_args() {
+                let bubs = &mut bubbles;
+                for b in bubs.iter_mut() {
+                    b.update();
+                }
+            }
+    }
+}
+
+pub fn generate_random_color() -> [f32; 4] {
+    [rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>()]
+}
+
+pub fn generate_bubbles(ammount: i32) -> Vec<Bubble> {
+    let mut bubbles: Vec<Bubble> = Vec::new();
+    for _ in 0..ammount {
+        bubbles.push(Bubble::new(
+            rand::random::<f64>() * WIDTH as f64,
+            rand::random::<f64>() * HEIGHT as f64,
+            rand::random::<f64>(),
+            rand::random::<f64>(),
+            rand::random::<f64>()
+        ));
+    }
+    bubbles
+}
